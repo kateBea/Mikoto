5766837ba363b432d8f77b390dd91b5deeecf7eb
kateBea <zanetty54@gmail.com>
2023-09-01 21:04:29 +0200

    removed unecessary shaders

--- engine/Utility/Common.hh
+++ engine/Utility/Common.hh
@@ -307,7 +307,7 @@ namespace Mikoto {
     inline auto GetFileData(const Path_T& path) -> std::string {
         std::ifstream file{ path, std::ios::binary };
         if (!file.is_open())
-            throw std::runtime_error("Failed to open SPR-V file");
+            throw std::runtime_error("Failed to open file");
 
         return std::string{ std::istreambuf_iterator<CharArray::value_type>(file), std::istreambuf_iterator<CharArray::value_type>() };
     }

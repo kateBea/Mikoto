c8849d6564193c56edd9faaa59c73c4c7886a3a6
ケイト <55351037+kateBea@users.noreply.github.com>
2025-01-23 00:22:44 +0100

    font fix

---
+++ Assets/shaders/opengl-glsl/debugShaderVert.glsl
@@ -0,0 +1,18 @@
+#version 430 core
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec3 a_Normal;
+layout(location = 2) in vec3 a_Color;
+layout(location = 3) in vec2 a_TextureCoordinates;
+
+out vec3 vertexColor;
+out vec2 vertexTexCoord;
+
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+uniform mat4 u_Transform;
+
+void main() {
+    vertexColor = a_Color;
+    vertexTexCoord = a_TextureCoordinates;
+    gl_Position = u_Projection * u_View * u_Transform * vec4(a_Position, 1.0);
+}

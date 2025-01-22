b4e0609de9acd8b6bdd74a30dac85473e2b9fdd8
ケイト <55351037+kateBea@users.noreply.github.com>
2025-01-22 23:28:18 +0100

    remove resources

--- Assets/shaders/vulkan-spirv/DeferredVertexShader.glsl
+++
@@ -1,9 +0,0 @@
-#version 450
-
-// Input data (geometry information)
-layout(location = 0) out vec2 textureCoordinates;
-
-void main() {
-    textureCoordinates = vec2((gl_VertexIndex << 1) & 2, gl_VertexIndex & 2);
-    gl_Position = vec4(textureCoordinates * 2.0f - 1.0f, 0.0f, 1.0f);
-}

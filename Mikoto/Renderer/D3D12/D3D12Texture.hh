b98e4a2a4d3a4dc4b2970a1219ae2e87b0c2114c
ケイト <zanetty54@gmail.com>
2025-11-23 12:58:33 +0100

    directx setup

---
+++ Mikoto/Renderer/D3D12/D3D12Texture.hh
@@ -0,0 +1,13 @@
+//
+// Created by kate on 11/23/25.
+//
+
+#ifndef MIKOTO_D3D12TEXTURE_HH
+#define MIKOTO_D3D12TEXTURE_HH
+
+
+class D3D12Texture {
+};
+
+
+#endif//MIKOTO_D3D12TEXTURE_HH
---
+++ Mikoto/Sources/D3D12Texture.cc
@@ -0,0 +1,5 @@
+//
+// Created by kate on 11/23/25.
+//
+
+#include "../Renderer/D3D12/D3D12Texture.hh"

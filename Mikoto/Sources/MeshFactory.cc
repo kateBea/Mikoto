77dbd86cd390560ac8eb9d37356a0828c28093ed
ケイト <zanetty54@gmail.com>
2025-11-28 09:40:36 +0100

    fix texture loading (texture coordinates)

--- Mikoto/Sources/MeshFactory.cc
+++ Mikoto/Sources/MeshFactory.cc
@@ -109,9 +109,9 @@ namespace Mikoto {
             }
 
             // Texture coordinates -----
-            if ( mesh->mTextureCoords[0] != nullptr ) {
+            if ( mesh->HasTextureCoords(0) ) {
                 vertices.emplace_back( mesh->mTextureCoords[0][index].x );
-                vertices.emplace_back( invertY ? -mesh->mTextureCoords[0][index].y : mesh->mTextureCoords[0][index].y );
+                vertices.emplace_back( 1 - mesh->mTextureCoords[0][index].y );
             } else {
                 vertices.emplace_back( 0.0f );
                 vertices.emplace_back( 0.0f );

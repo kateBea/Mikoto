a3fa45d80409ae4d18f7a41b45fff9aac54c1e14
ケイト <zanetty54@gmail.com>
2025-12-03 18:22:42 +0100

    fix example and feature check macro

--- Mikoto-Apps/Sockets/EntryPoint.cc
+++ Mikoto-Apps/Sockets/EntryPoint.cc
@@ -16,14 +16,15 @@ auto main( const int, char** ) -> int {
     SocketHandle socket{ ns.CreateSocket( SocketType::SOCKET_TCP, "localhost", 8000 ) };
     if (socket->IsConnected()) {
         MKT_CORE_LOGGER_DEBUG( "Connected to address" );
+
+        // This message should appear on the console or
+        // wherever the listener is writing incoming messages
+        // Will show a 400 with this message as this is not a proper HTTP request message
+        socket->SendSync( "Hello from Mikoto Sockets example" );
+
     } else {
         MKT_CORE_LOGGER_ERROR( "Failed to connect to address" );
     }
-
-    // This message should appear on the console or
-    // wherever the listener is writing incoming messages
-    // Will show a 400 with this message as this is not a proper HTTP request message
-    socket->SendSync( "Hello from Mikoto Sockets example" );
 
     // Free the handle
     socket.Reset();

0a7f71b03e576d77cc3b239d968d0bd2f0bcb3df
ケイト <zanetty54@gmail.com>
2026-01-19 10:46:44 +0100

    added minimal setup to use as playground with direct3d

--- Mikoto-Apps/HelloDirect3D12/EntryPoint.cc
+++ Mikoto-Apps/HelloDirect3D12/EntryPoint.cc
@@ -1,4 +1,4 @@
-//    Copyright 2025 ケイト
+//    Copyright 2026 ケイト
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
 // limitations under the License.
 
 #include <Logging/Logger.hh>
-#include <Networking/Server.hh>
-#include <Networking/NetworkService.hh>
 
 auto main( const int, char** ) -> int {
     using namespace Mikoto;

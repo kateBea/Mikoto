8968dd3716c43ff12e8a99925cd252ae2bc8bc3e
ケイト <zanetty54@gmail.com>
2026-01-26 11:04:39 +0100

    update platform window examples

--- Mikoto-Apps/WindowWin32/EntryPoint.cc
+++ Mikoto-Apps/WindowWin32/EntryPoint.cc
@@ -1,32 +1,47 @@
+//    Copyright 2026 ケイト
 //
-// Created by kate on 11/22/25.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #include <exception>
 
+#include <Core/Root.hh>
+#include <Core/Profiler.hh>
+
 #include <Platform/Window.hh>
+#include <Platform/WindowsService.hh>
 
 // Globals
 Mikoto::Window* g_Window{};
 
 auto InitializeWindow() -> void {
+    MKT_BEGIN_PROFILER_NAMED();
+
     using namespace Mikoto;
 
-    // Create a window
-    WindowProperties properties{};
-    properties.Resizable = true;
-    properties.Title = "Hello World";
-    properties.Backend = GraphicsAPI::DIRECTX_11;
-    properties.Width = 1280;
-    properties.Height = 720;
-
-    g_Window =  Window::Create( properties );
-
-    if ( g_Window ) {
-        g_Window->Init();
-    } else {
-        MKT_THROW_RUNTIME_ERROR( "Failed to create main application window!" );
+    // Initialize the window service so we can use Windows
+    if (!Root::RegisterService<WindowsService>( WindowsServiceCreateInfo{} )) {
+        return;
     }
+
+    WindowProperties properties{
+        .Title{ "Hello World" },
+        .Width{ 1280 },
+        .Height{ 720 },
+        .Backend{ GraphicsAPI::DIRECTX_11 },
+    };
+
+    g_Window = WindowsService::Get()->Create( properties );
 }
 
 auto RunCleanup() -> void {

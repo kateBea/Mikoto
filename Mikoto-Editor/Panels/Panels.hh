638fa75778436bb08be5f186e08eab71f1fabf40
ケイト <zanetty54@gmail.com>
2026-01-26 10:27:32 +0100

    group panels into master header

---
+++ Mikoto-Editor/Panels/Panels.hh
@@ -0,0 +1,31 @@
+//    Copyright 2026 ケイト
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MIKOTO_PANELS_HH
+#define MIKOTO_PANELS_HH
+
+#include <Panels/ScenePanel.hh>
+#include <Panels/StatsPanel.hh>
+#include <Panels/AssetsPanel.hh>
+#include <Panels/ConsolePanel.hh>
+#include <Panels/SettingsPanel.hh>
+#include <Panels/HierarchyPanel.hh>
+#include <Panels/InspectorPanel.hh>
+#include <Panels/GamePreviewPanel.hh>
+#include <Panels/LightingDebugPanel.hh>
+#include <Panels/PassVisualizerPanel.hh>
+#include <Panels/ContentBrowserPanel.hh>
+#include <Panels/ScenePropertiesPanel.hh>
+
+#endif //MIKOTO_PANELS_HH
---
+++ Mikoto/Core/Core.hh
@@ -0,0 +1,28 @@
+//    Copyright 2026 ケイト
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MIKOTO_CORE_HH
+#define MIKOTO_CORE_HH
+
+#include <Core/Profiler.hh>
+#include <Core/SystemStats.hh>
+
+#include <Core/CoreEvents.hh>
+
+#include <Core/LocalizationService.hh>
+
+#include <Core/InputService.hh>
+#include <Core/RuntimeConsole.hh>
+
+#endif // MIKOTO_CORE_HH

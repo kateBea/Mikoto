3fff2e5e5eb1a6db624cda0d69ba5eebddbcb362
ケイト <zanett54@gmail.com>
2026-02-12 01:42:55 +0100

    change default camera damping factor

--- Mikoto-Editor/Panels/SettingsPanel.hh
+++ Mikoto-Editor/Panels/SettingsPanel.hh
@@ -32,7 +32,7 @@ namespace Mikoto {
         float NearPlane{ 0.1f };
         float FarPlane{ 2000.0f };
         float FieldOfView{ 45.0f };
-        float DampingFactor{ 15.0f };
+        float DampingFactor{ 6.0f };
         bool WantXAxisRotation{ true };
         bool WantYAxisRotation{ true };
 

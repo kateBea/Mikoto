be64a7c4b0579efb040f9e465c2646c1b97c4244
ケイト <zanetty54@gmail.com>
2026-02-24 13:22:51 +0100

    text render slang helpers

--- Mikoto/Resources/Resources/Shaders/vulkan-spirv/material/Material.glsl
+++ Mikoto/Resources/Resources/Shaders/vulkan-spirv/material/Material.glsl
@@ -1,17 +1,20 @@
 struct ShaderMaterial {
+    // Factors
     vec4 BaseColorFactor;
     vec4 EmissiveFactor;
     vec4 DiffuseFactor;
     vec4 SpecularFactor;
 
-    float Workflow;
+    int Workflow;
 
+    // Parameters
     float MetallicFactor;
     float RoughnessFactor;
     float AlphaMask;
     float AlphaMaskCutoff;
     float EmissiveStrength;
 
+    // UV SET
     int BaseColorTextureSet;
     int PhysicalDescriptorTextureSet;
     int NormalTextureSet;
